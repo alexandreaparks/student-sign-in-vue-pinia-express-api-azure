c322df1f89bfa0880d5520b01dc36473147b68c5
alexandrea parks <alparks@idk.com>
2024-05-09 15:01:40 -0500

    vue requests -> express server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // any Vue requests with /api in the path will be sent to Express server (localhost:3000)
+      '/api': 'http://localhost:3000/'
+    }
+  }
 })
